bc461f57c05bc13ef213207ec38d5fb7d530744e
simonpartridge86 <simonpartridge86@gmail.com>
2022-11-02 13:49:37 +0000

    7kyu kata added

---
+++ 7kyu/fizzbuzz.js
@@ -0,0 +1,17 @@
+// kata link: https://www.codewars.com/kata/5300901726d12b80e8000498
+
+function fizzbuzz(n) {
+  let result = [];
+  for (let i = 1; i <= n; i++) {
+    if (i % 3 === 0 && i % 5 === 0) {
+      result.push("FizzBuzz");
+    } else if (i % 3 === 0) {
+      result.push("Fizz");
+    } else if (i % 5 === 0) {
+      result.push("Buzz");
+    } else {
+      result.push(i);
+    }
+  }
+  return result;
+}
